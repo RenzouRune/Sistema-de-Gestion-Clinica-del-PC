54ef5fbe84aaded94bce5d21b6a1029315394f18
robinson <robinson>
2025-09-14 18:38:42 -0300

    en este commit se trabajo en la app de diagnostico y se completo (falta bootstrap)

--- diagnostico/urls.py
+++ diagnostico/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('diagnostico/asignar/', views.asignar, name='asignar_estudiante'),
+    path('diagnostico/evaluar/', views.evaluar, name='evaluar'),
+    path('diagnostico/lista/', views.lista_diagnosticos, name='lista_diagnosticos'),
 ]
