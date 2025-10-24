8d65b30e500c9d3aa4b3b275eb8834a87cc9f940
Javier C. <nemu7945@gamil.com>
2025-10-24 19:48:58 -0300

    pequeños cambios en modelos y mejoras en bootstrap

--- recepcion/data_store.py
+++
@@ -1,8 +0,0 @@
-#data_store.py creado para almacenar datos en memoria y poder compartirlos entre vistas y hardcodeamos equipos de ejemplo
-equipos = [
-    {'nombre': 'Juan Vargas', 'tipo': 'Desktop', 'problema': 'No enciende'},
-    {'nombre': 'Maria Sanchez', 'tipo': 'Laptop', 'problema': 'Pantalla rota'},
-    {'nombre': 'Pedro Alvarado', 'tipo': 'Tablet', 'problema': 'Batería no carga'},
-    {'nombre': 'Ana Torres', 'tipo': 'All-in-One', 'problema': 'No reconoce teclado'},
-    {'nombre': 'Luis Fuentes', 'tipo': 'Notebook', 'problema': 'Se apaga solo'},
-]
